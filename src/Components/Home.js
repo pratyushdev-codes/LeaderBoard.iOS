2549daca778e54c2de3e5cb8e3e57e5940537e8b
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-25 20:47:41 +0530

    Add files via upload

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,38 +21,66 @@ function Home() {
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item mx-3"><a className="nav-link" style={{ fontWeight:"bold", color: "white" }}>iOS Club Website</a></li>
               <li className="nav-item mx-3"><a className="nav-link" style={{ fontWeight:"bold",color: "white" }}>About</a></li> 
-              
               <li className="nav-item mx-3"><a className="nav-link" style={{ fontWeight:"bold",color: "white" }}>Contact</a></li>
               <li className="nav-item mx-3"><a className="nav-link" style={{ fontWeight:"bold",color: "white" }}>Support</a></li>
-            
             </ul>
             <div className="ms-auto">
-              <button type="button" className="btn btn" style={{ backgroundColor: "#65A0FB", borderRadius: "20px", color: "white" }}>Leader Board</button>
+              <button type="button" className="btn btn-primary" style={{ borderRadius: "20px", backgroundColor: "#65A0FB", color: "white" }}>Leader Board</button>
             </div>
           </div>
         </div>
       </nav>
-      <div style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
+      <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '20px' }}>
         <img src="images/hero-shape-1.svg" alt="" style={{ position: 'absolute', top: 0, left: 0, width: '50%', height: '100%', objectFit: 'cover' }} />
         <img src="images/hero-shape-2.svg" alt="" style={{ position: 'absolute', top: 0, right: 0, width: '50%', height: '100%', objectFit: 'cover' }} />
-        <br/>
-        <br/>
-        <h1 style={{fontWeight:"bold"}}>Welcome to iOS Club's XXX event</h1>
-        <h4 style={{fontWeight:"bold",color:"grey"}}>Check your position on LeaderBoard</h4>
-        <br/>
-        <div className="card" style={{ width: '18rem' }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+        
+        <h1 style={{fontWeight:"bold", marginBottom: '20px'}}>Welcome to iOS Club's XXX event</h1>
+        <h4 style={{fontWeight:"bold",color:"grey", marginBottom: '40px'}}>Check your position on LeaderBoard</h4>
+
+        <div className="card mx-auto text-center" style={{ width: '18rem', borderRadius: "20px", boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', marginBottom: '20px' }}>
+          <div className="card-body" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <a href="#" className="btn btn-primary" style={{ borderRadius: "20px", fontFamily: 'PT Sans, sans-serif', margin: '10px' ,marginBottom: '20px'}}>
+              Position: 1
+            </a>
+            <h5 className="card-title" style={{color:'#65A0FB', fontWeight:"bold"}}>Team AZ CODERS</h5>
+            <p className="card-text" style={{fontFamily:'PT Sans, sans-serif', fontWeight:"bold", marginBottom: '10px'}}>Score: 20 Pts</p>
+          </div>
         </div>
-      </div>
+
+        <div className="card mx-auto text-center" style={{ width: '18rem', borderRadius: "20px", boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', marginBottom: '20px' }}>
+          <div className="card-body" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <a href="#" className="btn btn-primary" style={{ borderRadius: "20px", fontFamily: 'PT Sans, sans-serif', margin: '10px' ,marginBottom: '20px'}}>
+              Position: 1
+            </a>
+            <h5 className="card-title" style={{color:'#65A0FB', fontWeight:"bold"}}>Team AZ CODERS</h5>
+            <p className="card-text" style={{fontFamily:'PT Sans, sans-serif', fontWeight:"bold", marginBottom: '10px'}}>Score: 20 Pts</p>
+          </div>
+        </div>
+
+        <div className="card mx-auto text-center" style={{ width: '18rem', borderRadius: "20px", boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', marginBottom: '20px' }}>
+          <div className="card-body" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <a href="#" className="btn btn-primary" style={{ borderRadius: "20px", fontFamily: 'PT Sans, sans-serif', margin: '10px' ,marginBottom: '20px'}}>
+              Position: 1
+            </a>
+            <h5 className="card-title" style={{color:'#65A0FB', fontWeight:"bold"}}>Team AZ CODERS</h5>
+            <p className="card-text" style={{fontFamily:'PT Sans, sans-serif', fontWeight:"bold", marginBottom: '10px'}}>Score: 20 Pts</p>
+          </div>
+        </div>
+
+        <div className="card mx-auto text-center" style={{ width: '18rem', borderRadius: "20px", boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', marginBottom: '20px' }}>
+          <div className="card-body" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <a href="#" className="btn btn-primary" style={{ borderRadius: "20px", fontFamily: 'PT Sans, sans-serif', margin: '10px' ,marginBottom: '20px'}}>
+              Position: 1
+            </a>
+            <h5 className="card-title" style={{color:'#65A0FB', fontWeight:"bold"}}>Team AZ CODERS</h5>
+            <p className="card-text" style={{fontFamily:'PT Sans, sans-serif', fontWeight:"bold", marginBottom: '10px'}}>Score: 20 Pts</p>
+          </div>
+        </div>
+        
+
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
